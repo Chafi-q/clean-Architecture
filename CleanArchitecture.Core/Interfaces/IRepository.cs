226d68de320c17a3521d3a91b751d1a01fd18e84
Chafiq <chafik.m149@gmail.com>
2025-03-19 13:27:10 +0100

    implementing generic repository and dbContext

---
+++ CleanArchitecture.Core/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace CleanArchitecture.Core.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity:class // dynamic repository 
+    {
+        ValueTask<TEntity> AddAsync(TEntity entity);
+        ValueTask<TEntity> Read(string EntityId);
+
+        ValueTask<IEnumerable<TEntity>> ReadAll();
+
+        ValueTask<TEntity> UpdtaeAsync(TEntity entity);
+        ValueTask DeleteAsync(TEntity entity);
+
+
+    }
+}
+
+// réutilisabilité 
---
+++ CleanArchitecture.Infrastructure/Persistence/Repository.cs
@@ -0,0 +1,33 @@
+﻿using CleanArchitecture.Core.Interfaces
+
+
+namespace CleanArchitecture.Infrastructure.Persistence
+{
+    class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public ValueTask<TEntity> AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask DeleteAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<TEntity> Read(string EntityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<IEnumerable<TEntity>> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<TEntity> UpdtaeAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
